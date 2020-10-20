5817bae77939dfef303f66ed6c148705a3e5a953
ericsonmoreira <moreira.ericson@gmail.com>
2020-10-20 00:24:06 -0300

    add PanetonesList

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,15 @@ import Panetone from '../../@types/Panetone';
 import SideBar from '../../components/SideBar';
 import api from '../../services/api';
 
-import { Container, Content, PanetonesList } from './styles';
+import {
+  Container,
+  Content,
+  PanetonesList,
+  PanetoneItem,
+  GoToDetailsButton,
+} from './styles';
+
+import { FiMoreHorizontal } from 'react-icons/fi';
 
 const Home: React.FC = () => {
   const [panetones, setPanetones] = useState<Panetone[]>([]);
@@ -19,12 +27,16 @@ const Home: React.FC = () => {
       <SideBar />
       <Content>
         <h1>Catalottone</h1>
-        
-        {panetones.map((panetone) => (
-          <PanetonesList key={panetone.id}>
-            <pre>{JSON.stringify(panetone, null, 2)}</pre>
-          </PanetonesList>
-        ))}
+        <PanetonesList>
+          {panetones.map((panetone) => (
+            <PanetoneItem key={panetone.id}>
+              <strong>{panetone.name}</strong>
+              <GoToDetailsButton to={`/panetones/${panetone.id}`}>
+                <FiMoreHorizontal size={24} color="rgba(0,0,0, 0.8)" />
+              </GoToDetailsButton>
+            </PanetoneItem>
+          ))}
+        </PanetonesList>
       </Content>
     </Container>
   );
--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 import colors from '../../styles/colors';
@@ -25,5 +26,25 @@ export const PanetonesList = styled.div`
 `;
 
 export const PanetoneItem = styled.div`
-  
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: ${colors.quaternary};
+  padding: 32px;
+  border-radius: 20px;
+`;
+
+export const GoToDetailsButton = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;
+  background: transparent;
+  transition: background-color 0.2s;
+
+  :hover {
+    background: ${colors.secondary};
+  }
 `;
