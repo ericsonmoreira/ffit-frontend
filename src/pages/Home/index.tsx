7c873e7a48a3fc4589259caf74510736c0b7eaf5
ericsonmoreira <moreira.ericson@gmail.com>
2020-10-21 19:52:39 -0300

    add funcinality search name panetone

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import IPanetone from '../../@types/IPanetone';
 import SideBar from '../../components/SideBar';
 import api from '../../services/api';
@@ -19,6 +19,12 @@ const Home: React.FC = () => {
   const [loading, setLoading] = useState(true);
 
   const [panetones, setPanetones] = useState<IPanetone[]>([]);
+
+  const [textSearchInput, setTextSearchInput] = useState('');
+
+  function handleChangeTextSearchInput(event: ChangeEvent<HTMLInputElement>) {
+    setTextSearchInput(event.target.value);
+  }
 
   useEffect(() => {
     try {
@@ -41,21 +47,27 @@ const Home: React.FC = () => {
       <Content>
         <h1>Catalottone</h1>
         <PanetonesList>
-          <SearchInput type="text" placeholder="Busque pelo nome" /> 
-          {panetones.map((panetone) => (
-            <PanetoneItem key={panetone.id}>
-              <section>
-                <img src={panetone.url} alt="panetone" />
-                <PanetoneLabel>
-                  <strong>{panetone.name}</strong>
-                  <section>Marca: {panetone.marca.name}</section>
-                </PanetoneLabel>
-              </section>
-              <GoToDetailsButton to={`/panetones/${panetone.id}`}>
-                <FiMoreHorizontal size={24} color="rgba(0,0,0, 0.8)" />
-              </GoToDetailsButton>
-            </PanetoneItem>
-          ))}
+          <SearchInput
+            type="text"
+            placeholder="Busque pelo nome"
+            onChange={handleChangeTextSearchInput}
+          />
+          {panetones
+            .filter((panetone) => panetone.name.includes(textSearchInput))
+            .map((panetone) => (
+              <PanetoneItem key={panetone.id}>
+                <section>
+                  <img src={panetone.url} alt="panetone" />
+                  <PanetoneLabel>
+                    <strong>{panetone.name}</strong>
+                    <section>Marca: {panetone.marca.name}</section>
+                  </PanetoneLabel>
+                </section>
+                <GoToDetailsButton to={`/panetones/${panetone.id}`}>
+                  <FiMoreHorizontal size={24} color="rgba(0,0,0, 0.8)" />
+                </GoToDetailsButton>
+              </PanetoneItem>
+            ))}
         </PanetonesList>
       </Content>
     </Container>
