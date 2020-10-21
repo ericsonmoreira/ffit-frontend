7da0da040403e38e764078abb70a2b9e2c044e51
ericsonmoreira <moreira.ericson@gmail.com>
2020-10-21 19:59:17 -0300

    add component CreatePanetone

---
+++ src/pages/CreatePanetone/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const CreatePanetone: React.FC = () => {
+  return (
+    <Container>
+      <h1>CreatePanetone</h1>
+    </Container> 
+  );
+};
+
+export default CreatePanetone;
