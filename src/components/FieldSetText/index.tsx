353a9ab687535dc5c073860fe55d3993e7f1853c
ericsonmoreira <moreira.ericson@gmail.com>
2020-10-22 14:42:57 -0300

    adjust in layout input FieldSetText

--- src/components/FieldSetText/index.tsx
+++ src/components/FieldSetText/index.tsx
@@ -8,11 +8,17 @@ interface FieldSetTextProps {
   label: string;
 }
 
-const FieldSetText: React.FC<FieldSetTextProps> = ({ name, label, ...rest }) => {
+const FieldSetText: React.FC<FieldSetTextProps> = ({
+  name,
+  label,
+  ...rest
+}) => {
   return (
     <Container {...rest}>
-      <label htmlFor={name}>{label}</label>
-      <Field id={name} type="text" name={name} />
+      <section>
+        <label htmlFor={name}>{label}</label>
+        <Field id={name} type="text" name={name} />
+      </section>
       <ErrorMessage name={name} component="div" />
     </Container>
   );
--- src/components/FieldSetText/styles.tsx
+++ src/components/FieldSetText/styles.tsx
@@ -3,7 +3,36 @@ import styled from 'styled-components';
 import colors from '../../styles/colors';
 
 export const Container = styled.fieldset`
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+  border: none;
+
+  section {
+    display: flex;
+    align-items: center;
+
+    label {
+      margin-right: 10px;
+      color: ${colors.secondary};
+      font-size: 18px;
+      font-weight: 700;
+    }
+    input {
+      flex: 1;
+      height: 50px;
+      border-radius: 25px;
+      padding: 0 20px;
+      outline: none;
+      color: ${colors.secondary};
+      font-weight: 700;
+      border: none;
+    }
+  }
+
   div {
-    color: ${colors.error}
+    margin-top: 10px;
+    color: ${colors.error};
+    font-size: 16px;
   }
 `;
