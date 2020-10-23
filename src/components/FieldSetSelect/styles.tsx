8d8bea4cd8922ddee4e07632c56853de8da21b45
ericsonmoreira <moreira.ericson@gmail.com>
2020-10-22 22:29:01 -0300

    layout FieldSetSelect

--- src/components/FieldSetSelect/styles.tsx
+++ src/components/FieldSetSelect/styles.tsx
@@ -3,7 +3,21 @@ import styled from 'styled-components';
 import colors from '../../styles/colors';
 
 export const Container = styled.fieldset`
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+  border: none;
   div {
-    color: ${colors.error}
+    color: ${colors.error};
+  }
+
+  select {
+    padding: 10px;
+    height: 40px;
+    border-radius: 20px;
+    border: none;
+    font-size: 16px;
+    color: ${colors.primary};
+    font-weight: 600;
   }
 `;
